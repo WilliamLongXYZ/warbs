cd88260696fbb48b1d16010582103eb737a6d02f
William Long <will@williamlong.xyz>
2021-11-09 18:03:47 -0600

    Added task-spooler Added task-spooler to use with dwmblocks statusbar.

--- warbs.sh
+++ warbs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pacman -S doas vim mpv git github-cli zathura yay monero monero-gui bitcoin-cli bitcoin-qt base-devel pass brave-browser-beta newsboat ffmpeg 
-yay -S sc-im litecoin-qt litecoin-cli wownero-git wowlet-git zcash zecwallet zecwallet-lite electrum-cash electrum-dash
+yay -S sc-im litecoin-qt litecoin-cli wownero-git wowlet-git zcash zecwallet zecwallet-lite electrum-cash electrum-dash task-spooler
 
 pushd /usr/src/
 git clone https://github.com/lukesmithxyz/vul.git
