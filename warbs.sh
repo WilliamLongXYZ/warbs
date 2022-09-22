1754d2f4ffddc867b3a60beef78d1af603d23d3e
William Long <will@williamlong.xyz>
2022-09-21 20:12:14 -0500

    Did a bit of cleaning in larbs.sh, and renamed it.

--- warbs.sh
+++ warbs.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+# William's Auto Rice Boostrapping Script (WARBS)
+# forked by William Long <will@williamlong.xyz>
+# from
 # Luke's Auto Rice Boostrapping Script (LARBS)
 # by Luke Smith <luke@lukesmith.xyz>
 # License: GNU GPLv3
 
 ### OPTIONS AND VARIABLES ###
 
-#dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-dotfilesrepo="https://github.com/williamlongxyz/dotfiles.git" #ADDITION.NOLUKE
-#progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
-progsfile="https://raw.githubusercontent.com/WilliamLongXYZ/WARBS/master/progs.csv" #ADDITION.NOLUKE
+dotfilesrepo="https://github.com/williamlongxyz/dotfiles.git"
+progsfile="https://raw.githubusercontent.com/WilliamLongXYZ/WARBS/master/progs.csv"
 aurhelper="yay"
 repobranch="master"
 
@@ -27,7 +28,7 @@ error() {
 
 welcomemsg() {
 	whiptail --title "Welcome!" \
-		--msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
+		--msgbox "Welcome to William's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-William" 10 60
 
 	whiptail --title "Important Note!" --yes-button "All ready!" \
 		--no-button "Return..." \
@@ -206,7 +207,7 @@ allowdoas() {
 
 finalize() {
 	whiptail --title "All done!" \
-		--msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 13 80
+		--msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t William" 13 80
 }
 
 ### THE ACTUAL SCRIPT ###
